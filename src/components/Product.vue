78dde6f4e65780cbf6d4e66b65e8bb6aa59bb3da
Thandar Aung <thandaraung903@gmail.com>
2020-04-06 10:21:08 +0630

    product page for all responsive phone size (including ipad and ipad pro)

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -237,5 +237,139 @@ export default {
         }
         
     }
+
+    @media screen and (max-width: 1366px) {
+        .container {
+            width: 100%;
+            max-width: 100%;
+        }
+        .leftcol {
+            padding-left: 0px; 
+            padding-right: 0px; 
+            text-align: right;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+        .square-img {
+            width: 1.0625rem;
+            height: 1.0625rem;
+        }
+        .modifyrow {
+            padding-top: 20px;
+            padding-bottom: 10px;
+            background: #FFF;
+            border-top: 1rem solid #f0f0f0;
+        }
+        .horizontal-line {
+            border-top: 1px solid #f0f0f0;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        .modifyimg {
+            width: 5.625rem;
+            height: 5.625rem;
+        }
+        .rightcss {
+            text-align: left;
+            padding-left: 0.7rem;
+        } 
+        .p-bot {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #333333;
+        }
+        .p-bot-1 {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #999999;
+        }
+        .p-bot-2 {
+            margin-bottom: 3px;
+            font-size: 1rem;
+            color: #FF7800;
+        }
+        .header-css1 {
+            background: #FFF;
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+            border-bottom: 0.8rem solid #F1F1F1;
+        }
+        .circle-img {
+            width: 1.125rem;
+            height: 1.2375rem;
+            margin-right: 5px;
+        }
+        .tabActive {
+            color: #0FA9FE;
+            font-size: 0.9375rem;
+        }
+        .tabNoA {
+            color: #333333;
+            font-size: 0.9375rem;
+        }
+        .tabLine {
+            display: flex; 
+            justify-content: center; 
+        }
+        .tablinein {
+            background: #00A4FF; 
+            height: 3px; 
+            width: 65px;
+        }
+        .tablineinElse {
+            height: 3px; 
+            width: 65px;
+        }
+        .common-tab {
+            width: 5rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            margin-left: 0.5rem;
+            font-size:  0.875rem;
+            color: #999999;
+        }
+        .common-tab1 {
+            width: 4.8rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.875rem;
+            color: #999999;
+            margin-left: 0.6rem;
+        }
+        .common-tab2 {
+            width: 4.8rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.875rem;
+            color: #999999;
+            margin-left: 0.5rem;
+        }
+        .tabgroup {
+            display: flex; 
+            padding-left: 0px; 
+            padding-right: 15px;
+        }
+        .tabgroup1 {
+            display: flex; 
+            justify-content: flex-end;
+        }
+        .tabgroup2 {
+            display: flex; 
+            justify-content: flex-end;
+        }
+        .tabrow {
+            align-items: center;
+            padding: 0.7rem 0rem 0.7rem 0rem; 
+        }
+        
+    }
   
 </style>
