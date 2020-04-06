2fc0ab88345126d96e66ff76a75f3df9f053684b
Thandar Aung <thandaraung903@gmail.com>
2020-04-06 14:41:13 +0630

    message platform responsive

--- src/components/MessagePlatform.vue
+++ src/components/MessagePlatform.vue
@@ -217,5 +217,134 @@ export default {
         }
         
     }
+
+    @media screen and (max-width: 1366px) and (min-width: 768px) {
+        .container {
+            width: 100%;
+            max-width: 100%;
+        }
+        .circlecol {
+            padding-left: 10px;
+        }
+        .txtcss {
+            text-align: left;
+        }
+        .my-new {
+            
+        }
+        .text1 {
+            font-size: 1.0625rem;
+            color: #333333;
+        }
+        .text2 {
+            font-size: 0.75rem;
+            color: #A3A3A3;
+        }
+        .text3 {
+            font-size: 0.9375rem;
+            color: #A3A3A3;
+        }
+        .header-css {
+            background-image: linear-gradient(to right, #4EBDFF, #6093FF);
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        .header-css1 {
+            background: #FFF;
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+            /* padding-bottom: 10px; */
+        }
+        .body-css {
+            /* background: #FFF; */
+            padding-top: 15px;
+        }
+        .header-img {
+            width: 0.5625rem;
+            height: 1.03125rem;
+        }
+        .row-img {
+            width: 1.375rem;
+            height:  1.375rem;
+        }
+        .row1-txt {
+            margin-left: 10px; 
+            margin-right: 10px;
+            padding-right: 10px;
+        }
+        .row1-txt-inner {
+            display: flex; 
+            justify-content: space-between;
+        }
+        .divtopbot {
+            padding-top: 1rem;
+            padding-bottom: 2rem;
+        }
+        .row-horizontal-line {
+            background: #F0F0F0;
+        }
+        .horizontal-line {
+            width: 100%; 
+            height: 1px; 
+            margin-top: 10px;
+            background: #EFEFEE;
+        }
+        .row1 {
+            margin-top: -20px; 
+        }
+        .footerCss {
+            background: #F9F9F9;
+            margin-left: 0px;
+            margin-right: 0px;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+        }
+        .footerImg {
+            width: 40px; 
+            height: 40px;
+            margin-top: 0.5rem;
+        }
+        .tabActive {
+            color: #0FA9FE;
+        }
+        .tabNoA {
+            color: #333333;
+        }
+        .tabLine {
+            display: flex; 
+            justify-content: center; 
+        }
+        .tablinein {
+            background: #00A4FF; 
+            height: 3px; 
+            width: 65px;
+        }
+        .tablineinElse {
+            height: 3px; 
+            width: 65px;
+        }
+        .title-txt {
+            color: #FFF;
+            font-size: 1.03125rem;
+        }
+        .circle {
+            background: #5594EF;
+            width: 2.8125rem;
+            height: 2.8125rem;
+            border-radius: 1.40625rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .footerupdate {
+            background: lemonchiffon;
+        }
+        
+    }
   
 </style>
