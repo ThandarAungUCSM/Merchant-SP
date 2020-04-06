c92774c2e1faad4bf7707d27f98736c2188d8cd5
Thandar Aung <thandaraung903@gmail.com>
2020-04-06 11:28:24 +0630

    pending payment, deliver, order page for all phone size responsive

--- src/components/PendingPayment.vue
+++ src/components/PendingPayment.vue
@@ -609,5 +609,383 @@ export default {
        
         
     }
+
+
+    @media screen and (max-width: 1366px) {
+        .container {
+            width: 100%;
+            max-width: 100%;
+        }
+        .input-css {
+            border-radius: 30px;
+        }
+        .search-css {
+            text-align: left;
+            display: flex;
+            align-items: center;
+        }
+        .search-img {
+            width: 1.125rem;
+            height: 1.125rem;
+            position: absolute;
+        }
+        .left-arrow-pos {
+            text-align: left;
+        }
+        .header-css {
+            background: cyan;
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        
+        .leftcol {
+            padding-left: 0.3rem;
+            padding-right: 0px; 
+            text-align: right;
+        }
+       
+        .modifyrow {
+            padding-top: 20px;
+            padding-bottom: 10px;
+            background: #FFF;
+            border-top: 0.7rem solid #f0f0f0;
+        }
+        .horizontal-line {
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        .modifyimg {
+            width: 5.625rem;
+            height: 5.625rem;
+        }
+        .rightcss {
+            text-align: left;
+            padding-left: 1rem;
+            padding-right: 0rem;
+        } .p-bot {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #333333;
+        }
+        .p-bot-1 {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #999999;
+        }
+       
+        .header-css {
+            background-image: linear-gradient(to right, #4EBDFF, #6093FF);
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        .header-css1 {
+            background: #FFF;
+            display: flex;
+            align-items: center;
+            padding-top: 10px;
+        }
+        .header-img {
+            width: 20px;
+            height: 30px;
+        }
+        .header-img {
+            width: 0.5625rem;
+            height: 1.03125rem;
+        }
+      
+       
+       
+        .row1 {
+            margin-top: -20px; 
+        }
+        .footerCss {
+            background: #F9F9F9;
+            margin-left: 0px;
+            margin-right: 0px;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+        }
+        .footerImg {
+            width: 40px; 
+            height: 40px;
+            margin-top: 0.5rem;
+        }
+        .tabActive {
+            color: #0FA9FE;
+            font-size: 0.9375rem;
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+        .tabNoA {
+            color: #333333;
+            font-size: 0.9375rem;
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+        .tabLine {
+            display: flex; 
+            justify-content: center; 
+        }
+        .tablinein {
+            background: #00A4FF; 
+            height: 3px; 
+            width: 65px;
+        }
+        .tablineinElse {
+            height: 3px; 
+            width: 65px;
+        }
+        .title-txt {
+            color: #FFF;
+            font-size: 1.03125rem;
+        }
+      
+        .common-tab1 {
+            width: 5.375rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #666666;
+            margin-left: 0.7rem;
+        }
+        .common-tab1-color {
+            width: 5.375rem;
+            border: 1px solid #EC632C;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #EC632C;
+            margin-left: 0.7rem;
+        }
+       
+        .tabgroup1 {
+            display: flex; 
+            justify-content: flex-end;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* to be deliver css */
+        .delitabrow {
+            align-items: center;
+            padding: 0.7rem 0rem 0.7rem 0.5rem; 
+            border-top: 0.7rem solid #f0f0f0;
+            border-bottom: 1px solid #f0f0f0;
+        }
+        .delicircle-img {
+            width: 1.125rem;
+            height: 1.2375rem;
+            margin-right: 5px;
+        }
+        .delitabgroup {
+            display: flex; 
+            margin-left: 0.5rem; 
+        }
+        .delicommon-tab {
+            width: 5rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.875rem;
+            color: #999999;
+        }
+        .delicommon-tab1 {
+            width: 5.375rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #666666;
+            margin-left: 0.5rem;
+        }
+        .delicommon-tab1-color {
+            width: 5.375rem;
+            border: 1px solid #EC632C;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #EC632C;
+            margin-left: 0.5rem;
+        }
+       
+        .delitabgroup1 {
+            display: flex; 
+            justify-content: flex-end;
+        }
+        .delimodifyrow {
+            padding-top: 20px;
+            padding-bottom: 10px;
+            background: #FFF;
+        }
+        .delileftcol {
+            padding-left: 0px; 
+            padding-right: 0px; 
+            text-align: right;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
+        .delisquare-img {
+            width: 1.0625rem;
+            height: 1.0625rem;
+        }
+        .delimodifyimg {
+            width: 5.625rem;
+            height: 5.625rem;
+        }
+        .delirightcss {
+            text-align: left;
+            padding-left: 0.7rem;
+        } 
+        .delip-bot {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #333333;
+        }
+        .delip-bot-1 {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #999999;
+        }
+        .delihorizontal-line {
+            padding-top: 10px;
+            padding-bottom: 10px;
+            border-bottom: 0.8rem solid #f0f0f0;
+        }
+
+
+
+
+        /* order tab css */
+        .ordermodifyrow {
+            padding-top: 20px;
+            padding-bottom: 10px;
+            background: #FFF;
+            border-top: 0.7rem solid #f0f0f0;
+        }
+        .orderleftcol {
+            padding-left: 5px; 
+            padding-right: 0px; 
+            text-align: right;
+        }
+        .orderleftcol-new {
+            text-align: left;
+            font-size: 0.9375rem;
+            padding-right: 0rem;
+            /* padding-left: 5px; */
+        }
+        .ordermodifyimg {
+            width: 5.625rem;
+            height: 5.625rem;
+        }
+        .orderrightcss {
+            text-align: left;
+            padding-left: 1.3rem;
+        } 
+        .orderp-bot {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #333333;
+        }
+        .orderp-bot-1 {
+            margin-bottom: 3px;
+            font-size: 0.875rem;
+            color: #999999;
+        }
+        .orderhorizontal-line {
+            padding-top: 10px;
+            padding-bottom: 10px;
+            display: flex;
+            align-items: center;
+        }
+        .ordertabgroup1 {
+            display: flex; 
+            justify-content: flex-end;
+            align-items: center;
+        }
+        .ordercommon-tab {
+            width: 4.8rem;
+            border: 1px solid #EC632C;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.875rem;
+            color: #EC632C;
+            /* margin-left: 0.5rem; */
+        }
+        .ordercommon-tab11 {
+            width: 5.375rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #666666;
+            margin-right: 5px;
+        }
+        .ordercommon-tab1 {
+            width: 5.375rem;
+            border: 1px solid #999999;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #666666;
+            margin-left: 0.5rem;
+        }
+        .ordercommon-tab1-color {
+            width: 5.375rem;
+            border: 1px solid #EC632C;
+            border-radius: 2rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            font-size:  0.78rem;
+            color: #EC632C;
+            margin-left: 0.5rem;
+        }
+        .ordertext-css {
+            color: #0FA9FE;
+        }
+        .orderchtextimg {
+            width: 1.25rem;
+            height: 1.25rem;
+        }
+        .orderrightcol-new {
+            padding-left: 0px;
+            padding-right: 10px;
+        }
+       
+        
+    }
   
 </style>
